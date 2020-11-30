f51aa847863079d4dd0ba8ac5869279760a921d0
AndreAugustoAAQ <andreaugustoqueiroz999@gmail.com>
2020-11-30 20:51:52 -0300

    KDE Connect update and package fixes and improvements
    # KDE Connect updates
    ^ updated KDE Connect
    ^ updated kdeconnect-kde.nupkg
    ^ updated kdeconnect-kde\README.MD
    ^ updated Update.ps1
    * fixes on chocolateyInstall.ps1
    * replaced direct url to variable url in $packageArgs in chocolateyInstall.ps1
    - removed unescessary unzipLocation in $packageArgs
    + add checksum verification for 64 bit package on Update.ps1

--- kdeconnect-kde/Update.ps1
+++ kdeconnect-kde/Update.ps1
@@ -5,32 +5,29 @@ $artifacts64 = 'https://binary-factory.kde.org/view/Windows%2064-bit/job/kdeconn
 
 function global:au_GetLatest {  
   $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-  $url           = $download_page.links | ? href -match '.zip$' | select -First 1 -expand href
-  $kdeversion    = ($url -split '/|.zip' | select -Last 1 -Skip 1).Replace('v','')
+  $url64         = $download_page.links | ? href -match '.zip$' | select -First 1 -expand href
+  $kdeversion    = ($url64 -split '/|.zip' | select -Last 1 -Skip 1).Replace('v','')
   $version       = ((Invoke-WebRequest $artifacts64 -UseBasicParsing).links | ? href -match '.exe$' | select -First 1 -expand href) -split '-' | select -First 1 -Skip 2
   $build64       = ((Invoke-WebRequest $artifacts64 -UseBasicParsing).links | ? href -match '.exe$' | select -First 1 -expand href) -split '-' | select -First 1 -Skip 3
 
     @{
-        URL64        = 'https://binary-factory.kde.org/view/Windows%2064-bit/job/kdeconnect-kde_Release_win64/'+ $build64 +'/artifact/kdeconnect-kde-' + $version + '-' + $build64 + '-windows-msvc2019_64-cl.exe'
-        Version      = $version
-        ReleaseNotes = 'https://kde.org/announcements/changelog-releases.php?version=' + $kdeversion + '#kdeconnect-kde'
+        URL64        	= 'https://binary-factory.kde.org/view/Windows%2064-bit/job/kdeconnect-kde_Release_win64/'+ $build64 +'/artifact/kdeconnect-kde-' + $version + '-' + $build64 + '-windows-msvc2019_64-cl.exe'
+        Version      	= $version + '.' + $build64
+		ChecksumType64	= 'sha256'
     }
-
 }
 
 function global:au_SearchReplace {
         @{
-        "kdeconnect.nuspec" = @{
-            "(\<releaseNotes\>).*(\<\/releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
-            }
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url64bit\s*=\s*`").*" = "`${1}$($Latest.URL64)`""
-            }
+            "(^[$]url64\s*=\s*)('.*')"      = "`$1'$($Latest.URL64)'"
+			"(?i)(^\s*checksum64\s*=\s*)('.*')"   = "`$1'$($Latest.Checksum64)'"
+			}
         }
 }
 
 try {
-    update -ChecksumFor none
+    update -ChecksumFor 64
 } catch {
     $ignore = 'Not Found'
     if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
--- kdeconnect-kde/tools/ChocolateyInstall.ps1
+++ kdeconnect-kde/tools/ChocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = 'https://binary-factory.kde.org/view/Windows%2064-bit/job/kdeconnect-kde_Release_win64/422/artifact/kdeconnect-kde-1.4-422-windows-msvc2019_64-cl.exe'
 
 $packageArgs = @{
-  packageName   = 'kdeconnect-kde'
-  unzipLocation = $toolsDir
+  packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
-  url64bit      = 'https://binary-factory.kde.org/job/kdeconnect-kde_Release_win64/lastSuccessfulBuild/artifact/kdeconnect-kde-1.4-418-windows-msvc2019_64-cl.exe'
+  url64bit      = $url64
   softwareName  = 'KDE Connect*'
-  checksum64    = '47EA14BCB2BF62880B23CCDEA8A931BAF5276D89605F6F23FA9888A20A2D6AC7'
+  checksum64	= 'baee9324e592b08db85edddcb9b5aeb880f6cd1053bbe3d817ecb4450e52eddf'
   checksumType64= 'sha256'
   silentArgs    = '/S'
 }
