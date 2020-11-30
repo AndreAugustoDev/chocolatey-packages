0b210bd2e68486ebf82d01500a34ef3f047c0c4a
AndreAugustoAAQ <andreaugustoqueiroz999@gmail.com>
2020-11-30 14:22:37 -0300

    Installation fixes
    * fix installation method
    - removed 7zip dependency

--- photogimp/tools/chocolateyInstall.ps1
+++ photogimp/tools/chocolateyInstall.ps1
@@ -1,26 +1,19 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/Diolinux/PhotoGIMP/releases/download/1.0/PhotoGIMP.by.Diolinux.v2020.for.Flatpak.zip'
+$GIMPdata   = "$env:APPDATA\GIMP"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = "$toolsDir\pkg"
-  url           = $url
-  softwareName  = 'PhotoGIMP'
-  checksum      = '579DB0BE5FE4AC6669555F3C433D70FAA10D27527FDC119D3A464A2E2A729FAD'
-  checksumType  = 'sha256'
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = "$env:APPDATA\GIMP"
+  url            = $url
+  softwareName   = 'PhotoGIMP'
+  checksum       = '579DB0BE5FE4AC6669555F3C433D70FAA10D27527FDC119D3A464A2E2A729FAD'
+  checksumType   = 'sha256'
+  SpecificFolder = "PhotoGIMP by Diolinux v2020 for Flatpak\.var\app\org.gimp.GIMP\config\GIMP\"
 }
 
-$downloadArgs = @{
-	packageName		= $env:ChocolateyPackageName
-	fileFullPath	= "$toolsDir\pkg.zip"
-	url 			= $url
-}
-
-Get-ChocolateyWebFile @downloadArgs
-
-7z x "$toolsDir\pkg.zip" -o"$env:TEMP\chocolatey\photogimp" -y -bso0 -bd -aoa
-$path = "PhotoGIMP by Diolinux v2020 for Flatpak\.var\app\org.gimp.GIMP\config\GIMP\2.10\*"
-Write-Host "PhotoGIMP patch extracted to $env:APPDATA\GIMP\2.10"
-Copy-Item -Path "$env:TEMP\chocolatey\photogimp\$path" -Destination "$env:APPDATA\GIMP\2.10" -Recurse -Force
-Remove-Item -Recurse -Force -Path "$env:TEMP\chocolatey\photogimp"
+Install-ChocolateyZipPackage @packageArgs
+md -Force "$GIMPdata\2.10" | Out-Null
+Copy-Item  -Recurse -Force -Path "$GIMPdata\PhotoGIMP by Diolinux v2020 for Flatpak\.var\app\org.gimp.GIMP\config\GIMP\2.10\*" -Destination "$GIMPdata\2.10" | Out-Null
+Remove-Item -Recurse -Force -Path "$GIMPdata\PhotoGIMP by Diolinux v2020 for Flatpak"
