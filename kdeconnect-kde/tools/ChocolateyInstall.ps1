f85c43d3d3d3629a875ca2d02555c2db7fbf2ebc
AndreAugustoAAQ <andreaugustoqueiroz999@gmail.com>
2020-12-03 13:00:41 -0300

    # KDE Connect update
    ^ update KDE Connect to last successful build (422>424)
    - removed '(Install)' from kdeconnect-kde title
    - removed kdeconnect-kde/build.ps1

--- kdeconnect-kde/tools/ChocolateyInstall.ps1
+++ kdeconnect-kde/tools/ChocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://binary-factory.kde.org/view/Windows%2064-bit/job/kdeconnect-kde_Release_win64/422/artifact/kdeconnect-kde-1.4-422-windows-msvc2019_64-cl.exe'
+$url64      = 'https://binary-factory.kde.org/view/Windows%2064-bit/job/kdeconnect-kde_Release_win64/424/artifact/kdeconnect-kde-1.4-424-windows-msvc2019_64-cl.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
   url64bit      = $url64
   softwareName  = 'KDE Connect*'
-  checksum64	= 'baee9324e592b08db85edddcb9b5aeb880f6cd1053bbe3d817ecb4450e52eddf'
+  checksum64	= 'e83eefba341130be6e9a3ca85747330a9461f79d2ef9194df88329344a45a6d6'
   checksumType64= 'sha256'
   silentArgs    = '/S'
 }
