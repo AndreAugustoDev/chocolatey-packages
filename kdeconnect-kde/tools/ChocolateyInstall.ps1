62f7a9ba89b053685284f01e2b99dc9b58deb074
Andr├⌐ Augusto <andreaugustoqueiroz999@gmail.com>
2021-06-02 00:02:45 +0000

    AU: 1 updated - kdeconnect-kde [skip ci] https://gist.github.com/504f8c83b21394b7dbf71b9da54e191c/e29684b9a816316cf46c759c81fe8bee9d3df4ce

--- kdeconnect-kde/tools/ChocolateyInstall.ps1
+++ kdeconnect-kde/tools/ChocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64	= 'https://binary-factory.kde.org/job/kdeconnect-kde_Release_win64/507/artifact/kdeconnect-kde-1.4-507-windows-msvc2019_64-cl.exe'
+$url64	= 'https://binary-factory.kde.org/job/kdeconnect-kde_Release_win64/607/artifact/kdeconnect-kde-21.04.1-607-windows-msvc2019_64-cl.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'exe'
   url     		= $url64
   softwareName  = 'kdeconnect*'
-  checksum64	= '8f68566ed7219ba5f2a69087f2d1e0cf386f93e3d39992c9aa8aa6d67e071d4b'
+  checksum64	= 'c4cdb36e4c24c62f22906ec128f5f36a045b2136483826b60cd29904f6d883d2'
   checksumType64= 'sha256'
   silentArgs    = '/S'
 }
