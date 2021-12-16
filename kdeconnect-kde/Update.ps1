1c70330562d64c22c952f4c07ff1814a236053b3
André Augusto <andreaugustoqueiroz999@gmail.com>
2021-12-16 01:43:06 -0300

    Update Update.ps1
    * Fix KDE Connect update script issue (see https://github.com/majkinetor/au/issues/182)

--- kdeconnect-kde/Update.ps1
+++ kdeconnect-kde/Update.ps1
@@ -3,7 +3,6 @@ Import-Module au
 $releases = 'https://invent.kde.org/network/kdeconnect-kde/-/tags'
 $artifacts64 = 'https://binary-factory.kde.org/job/kdeconnect-kde_Release_win64/lastSuccessfulBuild/artifact/'
 
-
 function global:au_SearchReplace {
         @{
         ".\tools\chocolateyInstall.ps1" = @{
@@ -14,20 +13,25 @@ function global:au_SearchReplace {
 }
 
 function global:au_GetLatest {
-	$download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-	$url64         = $download_page.links | ? href -match '.zip$' | select -First 1 -expand href
-	$kdeversion    = ($url64 -split '/|.zip' | select -Last 1 -Skip 1).Replace('v','')
-	$version       = ((Invoke-WebRequest $artifacts64 -UseBasicParsing).links | ? href -match '.exe$' | select -First 1 -expand href) -split '-' | select -First 1 -Skip 2
-	$build64       = ((Invoke-WebRequest $artifacts64 -UseBasicParsing).links | ? href -match '.exe$' | select -First 1 -expand href) -split '-' | select -First 1 -Skip 3
+	$download_page     = Invoke-WebRequest -Uri $releases -UseBasicParsing
+	$artifacts64_page  = Invoke-WebRequest $artifacts64 -UseBasicParsing
+	$url64             = $download_page.links | ? href -match '.zip$' | select -First 1 -expand href
+	$kdeversion        = ($url64 -split '/|.zip' | select -Last 1 -Skip 1).Replace('v','')
+	$version           = ($artifacts64_page.links | ? href -match '.exe$' | select -First 1 -expand href) -split '-' | select -First 1 -Skip 2
+	$build64           = ($artifacts64_page.links | ? href -match '.exe$' | select -First 1 -expand href) -split '-' | select -First 1 -Skip 3
 
     return @{
 		URL64        	= 'https://binary-factory.kde.org/job/kdeconnect-kde_Release_win64/'+ $build64 +'/artifact/kdeconnect-kde-' + $version + '-' + $build64 + '-windows-msvc2019_64-cl.exe';
         Version      	= $version + '.' + $build64;
-		}
+    }
+}
+
+function global:au_BeforeUpdate {
+    $Latest.Checksum64 = Get-RemoteChecksum $Latest.URL64
 }
 
 try {
-    update -ChecksumFor 64
+    update -ChecksumFor none
 } catch {
     $ignore = 'Not Found'
    if ($_ -match $ignore) { Write-Host $ignore; 'ignore' }  else { throw $_ }
