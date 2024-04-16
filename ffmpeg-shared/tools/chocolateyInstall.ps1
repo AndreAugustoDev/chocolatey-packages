28d7162b3f30b80632c3b915925421e927bb8ba3
AndreAugustoDev <andreaugustoqueiroz999@gmail.com>
2024-04-16 16:49:30 -0300

    🐛 fix: ffmpeg-shared package generation

--- ffmpeg-shared/tools/chocolateyInstall.ps1
+++ ffmpeg-shared/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 ﻿$ErrorActionPreference  = 'Stop';
-$toolsDir               = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$toolsPath              = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 $version	              = '7.0'
 $url                    = 'https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full-shared.7z'
 
@@ -7,8 +7,8 @@ $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   unzipLocation  = $toolsPath
   url            = $url
-  checksum64     = '19f7ddacc3795d1168f14843d3233161dc474df4b981cc6158da614d0e00e339'
-  checksumType64 = 'sha256'
+  checksum       = '19f7ddacc3795d1168f14843d3233161dc474df4b981cc6158da614d0e00e339'
+  checksumType   = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
