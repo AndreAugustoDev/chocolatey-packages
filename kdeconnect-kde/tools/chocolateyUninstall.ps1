bb0e7f86f0cf68b6eaa0f55a6962063454c746de
AndreAugustoDev <andreaugustoqueiroz999@gmail.com>
2024-04-16 20:00:45 -0300

    🩹 chore: update kdeconnect uninstall

--- kdeconnect-kde/tools/chocolateyUninstall.ps1
+++ kdeconnect-kde/tools/chocolateyUninstall.ps1
@@ -1,33 +1,33 @@
-$ErrorActionPreference = 'Stop';
-
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'kdeconnect*'
-  fileType      = 'exe'
+  packageName  = $env:ChocolateyPackageName
+  softwareName = 'kdeconnect*'
+  fileType     = 'exe'
   silentArgs   = '/S'
 }
 
 $uninstalled = $false
-[array]$key = Get-UninstallRegistryKey -SoftwareName $packageArgs['softwareName']
+
+[array]$key = Get-UninstallRegistryKey @packageArgs
 
 if ($key.Count -eq 1) {
-  $key | % { 
+  $key | % {
     $packageArgs['file'] = "$($_.UninstallString)"
     
     if ($packageArgs['fileType'] -eq 'MSI') {
       $packageArgs['silentArgs'] = "$($_.PSChildName) $($packageArgs['silentArgs'])"
       
       $packageArgs['file'] = ''
-    } else {
     }
 
     Uninstall-ChocolateyPackage @packageArgs
   }
-} elseif ($key.Count -eq 0) {
+}
+elseif ($key.Count -eq 0) {
   Write-Warning "$packageName has already been uninstalled by other means."
-} elseif ($key.Count -gt 1) {
+}
+elseif ($key.Count -gt 1) {
   Write-Warning "$($key.Count) matches found!"
   Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
-  Write-Warning "Please alert package maintainer the following keys were matched:"
-  $key | % {Write-Warning "- $($_.DisplayName)"}
+  Write-Warning "Please alert the package maintainer that the following keys were matched:"
+  $key | % { Write-Warning "- $($_.DisplayName)" }
 }
