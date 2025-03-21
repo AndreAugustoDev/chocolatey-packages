71505ca2df90a197aae2a9b0d4b630881cdc702b
AndreAugustoDev <25272842+AndreAugustoDev@users.noreply.github.com>
2025-03-21 09:43:42 -0300

    🐛 fix(photogimp): update PhotoGIMP install script

--- photogimp/tools/chocolateyInstall.ps1
+++ photogimp/tools/chocolateyInstall.ps1
@@ -3,16 +3,12 @@ $url = 'https://github.com/Diolinux/PhotoGIMP/releases/download/3.0/PhotoGIMP.zi
 $GIMPdata = "$env:APPDATA\GIMP"
 
 $packageArgs = @{
-  packageName    = $env:ChocolateyPackageName
-  unzipLocation  = "$env:APPDATA\GIMP"
-  url            = $url
-  softwareName   = 'PhotoGIMP'
-  checksum       = '3CDC73D36851923B25E55388ECD400AE9A987C7D4F5D882E48F857EE8044DDF6'
-  checksumType   = 'sha256'
-  SpecificFolder = "3.0"
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $GIMPdata
+  url           = $url
+  softwareName  = 'PhotoGIMP'
+  checksum      = '3CDC73D36851923B25E55388ECD400AE9A987C7D4F5D882E48F857EE8044DDF6'
+  checksumType  = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
-New-Item -Path "$GIMPdata\3.0" -Force | Out-Null
-Copy-Item  -Recurse -Force -Path "$GIMPdata\*" -Destination "$GIMPdata\3.0" | Out-Null
-Remove-Item -Recurse -Force -Path "$GIMPdata"
